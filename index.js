80eae87fa3d34b6433f79a137a16ba97e78a3177
Dan Rufener <dan@dan.dan>
2020-02-06 18:21:51 -0800

    leaves

--- index.js
+++ index.js
@@ -29,8 +29,46 @@ stem = (x, y, length, r) => {
     return stemPieces
 }
 
+leaf = (x, y, r, direction) => {
+    var pieces = []
+    var radius = 2
+    for (var i = 0; i < r && radius > 1; i+=2) {
+        if (direction > 0) {
+            pieces.push({x: x + direction * i - r + 2, y, r: radius, color: 'green', border: 'green'})
+        } else {
+            pieces.push({x: x + direction * i + r - 2, y, r: radius, color: 'green', border: 'green'})
+        }
+        if (i < r / 3.5) {
+            radius += 2
+        } else {
+            radius -= 1
+        }
+    }
+    return pieces
+}
+
+leaves = (x, y, r, height) => {
+    var leafs = []
+    const max = height / (r + 10)
+    var num = Math.round(3 + Math.random() * (max - 3))
+    for (var i = 2; i < num; i++) {
+        const leafY = y + height * (i / num)
+        if (Math.random() < 0.3) {
+            leafs = leafs.concat(leaf(x, leafY, r, -1))
+        }
+    }
+    var num = Math.round(3 + Math.random() * (max - 3))
+    for (var i = 2; i < num; i++) {
+        const leafY = y + height * (i / num)
+        if (Math.random() < 0.3) {
+            leafs = leafs.concat(leaf(x, leafY, r, 1))
+        }
+    }
+    return leafs
+}
+
 flower = (x, y, r, length) => {
-    return stem(x, y, length - 2 * r, r / 20).concat(bud(x, y, r))
+    return stem(x, y, length - 2 * r, r / 20).concat(bud(x, y, r)).concat(leaves(x,y,r, length - 2 * r))
 }
 
 draw = () => {
@@ -45,7 +83,6 @@ draw = () => {
     const flowerLength = (document.getElementById("flowers").offsetHeight)
     var data = []
     for (var i = 0; i < 12; i++) {
-        console.log("HERE", i)
         data = data.concat(flower(flowerR + i * (2 * flowerR + 10) + 10, 200, flowerR, flowerLength))
     }
     console.log(data)
