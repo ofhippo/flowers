5dcd845f12996c42c64e70798a3eba059736854c
Dan Rufener <dan@dan.dan>
2020-02-06 17:00:33 -0800

    buds

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 
-draw = () => {
+bud = (x, y, r) => {
+    const startX = x
+    const startY = y
+    var pedals = []
+    while (r >= 0) {
+        pedals.push({x, y, r, color: 'red'})
+        r -= 3
+        if (Math.random() > 0.7) {
+            x += r / 10
+        } else {
+            x = startX
+        }
+        if (Math.random() > 0.7) {
+            y += r / 10
+        } else {
+            y = startY
+        }
+    }
 
+    return pedals
+}
+
+draw = () => {
     var svg = d3.select("#flowers")
                 .append("svg")
                 .style("width", "100%")
@@ -8,30 +29,24 @@ draw = () => {
                 .style("background-color", "black")
                 .append("g")
 
-                var json = {
-                    data: [
-                      { radius: 15, color: 'red' },
-                      { radius: 25, color: 'blue' },
-                      { radius: 5, color: 'yellow' }
-                    ]
-                  };
-
+        data = []
+        data = data.concat(bud(100, 200, 50))
+        console.log(data)
         svg.selectAll('circle')
-        .data(json.data)
+        .data(data)
         .enter()
         .append('circle')
         .attr('r', function (d) {
-            return d.radius;
+            return d.r
         })
-        .attr('cy', function (d, i) {
-            return 25;
+        .attr('cy', function (d) {
+            return d.y
         })
-        .attr('cx', function (d, i) {
-            var margin = 15,
-                minWidth = 30;
-            return (i * minWidth) + margin;
+        .attr('cx', function (d) {
+            return d.x
         })
         .attr('fill', function (d) {
             return d.color;
-        });
+        })
+        .style("stroke", "black")
 }
