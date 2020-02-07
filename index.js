57625a2320dfdf4ec5e30405e3313d23beb7ead2
Dan Rufener <dan@dan.dan>
2020-02-06 17:20:36 -0800

    stems

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
 bud = (x, y, r) => {
     const startX = x
     const startY = y
+    const startR = r
     var pedals = []
     while (r >= 0) {
-        pedals.push({x, y, r, color: 'red'})
-        r -= 3
+        pedals.push({x, y, r, color: 'red', border: "black"})
+        r -= Math.random() * (startR / 10)
         if (Math.random() > 0.7) {
             x += r / 10
         } else {
@@ -21,6 +21,18 @@ bud = (x, y, r) => {
     return pedals
 }
 
+stem = (x, y, length, r) => {
+    var stemPieces = []
+    for (var i = 0; i < length; i++) {
+        stemPieces.push({x, y: y + i, r, color: 'green', border: 'green'})
+    }
+    return stemPieces
+}
+
+flower = (x, y, r, length) => {
+    return stem(x, y, length - 2 * r, r / 20).concat(bud(x, y, r))
+}
+
 draw = () => {
     var svg = d3.select("#flowers")
                 .append("svg")
@@ -29,24 +41,29 @@ draw = () => {
                 .style("background-color", "black")
                 .append("g")
 
-        data = []
-        data = data.concat(bud(100, 200, 50))
-        console.log(data)
-        svg.selectAll('circle')
-        .data(data)
-        .enter()
-        .append('circle')
-        .attr('r', function (d) {
-            return d.r
-        })
-        .attr('cy', function (d) {
-            return d.y
-        })
-        .attr('cx', function (d) {
-            return d.x
-        })
-        .attr('fill', function (d) {
-            return d.color;
-        })
-        .style("stroke", "black")
+    const flowerR = (document.getElementById("flowers").offsetWidth / 24 - 5)
+    const flowerLength = (document.getElementById("flowers").offsetHeight)
+    var data = []
+    for (var i = 0; i < 12; i++) {
+        console.log("HERE", i)
+        data = data.concat(flower(flowerR + i * (2 * flowerR + 10) + 10, 200, flowerR, flowerLength))
+    }
+    console.log(data)
+    svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('r', function (d) {
+        return d.r
+    })
+    .attr('cy', function (d) {
+        return d.y
+    })
+    .attr('cx', function (d) {
+        return d.x
+    })
+    .attr('fill', function (d) {
+        return d.color;
+    })
+    .style("stroke", d => d.border)
 }
